62d3a3788868d4ee2eeb8d041681964821bc0036
Nguyễn Văn Giàu <75024999+vangiaurecca@users.noreply.github.com>
2021-10-18 11:57:12 +0700

    Update getExampleTasks.js

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("/todo-list-react/exampleTasks.json");
+    const response = await fetch("/TodoApp_Timeline/exampleTasks.json");
 
     if (!response.ok) {
         new Error(response.statusText);
